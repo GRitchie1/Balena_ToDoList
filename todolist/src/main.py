c26ecc3879e028c6fb8d025dd34bd8fbff755d8b
GRitchie1 <gregor.homeautomation@gmail.com>
2021-09-11 12:56:02 +0100

    Add Random item function

--- todolist/src/main.py
+++ todolist/src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from list_items import List_Items
+import random
 
 app = Flask('__name__')
 app.config['SECRET_KEY'] = 'SECRET_PROJECT'
@@ -7,6 +8,7 @@ app.config['SECRET_KEY'] = 'SECRET_PROJECT'
 
 lists= List_Items()
 categories = {"todo":"To Do","complete":"Complete",}
+random_item = []
 
 COMP_ACTION = "Y"
 UNCOMP_ACTION = "X"
@@ -18,8 +20,16 @@ def toDo():
   return render_template("index.html", name=name, categories = categories)
 
 
+@app.route("/<category>/<int:rand>", methods=["GET"])
+def get_random(category,rand=0):
+    list_items = lists.get_list_by_category(category)
+    if rand == 1:
+        global random_item
+        random_item = random.choice(list_items)
+    return redirect(url_for("items",category=category))
+
 @app.route("/<category>", methods=["GET", "POST"])
-def locations(category):
+def items(category):
   list_items = lists.get_list_by_category(category)
 
   if request.method == "POST":
@@ -32,7 +42,9 @@ def locations(category):
     elif action == UNCOMP_ACTION:
       lists.uncomplete(name)
 
-  return render_template("lists.html",category=category, categories=categories,list_items=list_items)
+
+  return render_template("lists.html",category=category, categories=categories,list_items=list_items,random_item=random_item)
+
 
 
 if __name__ == '__main__':
